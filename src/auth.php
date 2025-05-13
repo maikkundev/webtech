884051714b6354a6de3dce67f9e8791fff468585
Eleni Stylou <eleni.st15@hotmail.com>
2025-05-13 13:15:44 +0300

    add css to auth.php page

--- src/auth.php
+++ src/auth.php
@@ -61,6 +61,7 @@ if (isset($_POST['signup'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Authentication Form</title>
 </head>
 
